63d2c04216ed4a07264544ab80b2c716ed0e669b
Manuel Gruber <grubermk97@gmail.com>
2023-01-28 12:09:59 +0100

    solved 6 7

---
+++ 006/Program.cs
@@ -0,0 +1,7 @@
+﻿int sum = 0, squaresum = 0;
+for (int i = 1; i < 101; i++)
+{
+    sum += i;
+    squaresum += (int)Math.Pow(i, 2);
+}
+Console.WriteLine($"Difference: {Math.Pow(sum, 2) - squaresum}");
