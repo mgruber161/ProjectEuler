9665bc6932203a906f9bdcb050656d31d1bac59a
Manuel <grubermk97@gmail.com>
2023-01-29 19:06:58 +0100

    added EulerLib class library for common methods. solved 009 and 010.

---
+++ 009/Program.cs
@@ -0,0 +1,6 @@
+﻿for (int a = 0; a < 1000; a++)
+    for (int b = 0;b < 1000; b++)
+        for (int c = 0;c < 1000; c++)
+            if(a + b + c == 1000)
+                if(a*a + b*b == c*c)
+                    Console.WriteLine($"{a*b*c}");
---
+++ 010/Program.cs
@@ -0,0 +1,6 @@
+﻿EulerLib.EulerHelper eulerHelper = new EulerLib.EulerHelper();
+long sum = 0;
+for (int i = 1;i < 2_000_000; i++)
+    if(eulerHelper.IsPrime(i))
+        sum += i;
+Console.WriteLine(sum);
