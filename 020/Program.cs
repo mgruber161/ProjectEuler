7dbbe28b4ca73477c1b46900bfe638b55a88bb32
Manuel <grubermk97@gmail.com>
2023-02-01 16:39:46 +0100

    solved 020

---
+++ 020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+BigInteger big = FactorialNumber(100);
+long sum = 0;
+foreach (var character in big.ToString())
+	sum += character - '0';
+Console.WriteLine(sum);
+BigInteger FactorialNumber(int v)
+{
+	BigInteger res = new BigInteger(1);
+	for (int i = 1; i < v; i++)
+		res *= i;
+	return res;
+}
