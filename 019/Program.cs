004b6d9b4fab123b9ce4fe69c2a4daacd6b052e3
Manuel <grubermk97@gmail.com>
2023-02-01 16:31:26 +0100

    solved 019

---
+++ 019/Program.cs
@@ -0,0 +1,8 @@
+﻿var dt = new DateTime(1901, 01, 01);
+int count = 0;
+while(dt.Year < 2001)
+{
+    if(dt.DayOfWeek== DayOfWeek.Sunday && dt.Day == 1) count++; 
+    dt = dt.AddMonths(1);
+}
+Console.WriteLine(count);
