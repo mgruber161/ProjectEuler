14b93072ea7c83b5c424227e5afcb3d9ba2975da
Manuel <grubermk97@gmail.com>
2023-02-01 21:34:43 +0100

    solved 022

---
+++ 001-099/022/Program.cs
@@ -0,0 +1,10 @@
+﻿var names = File.ReadAllText(@"D:\repos\csharp\ProjectEuler\001-099\022\names.txt").Replace("\"", "").Split(',').OrderBy(n => n).ToArray();
+int sum = 0, score = 0;
+for(int i = 0; i < names.Length; i++)
+{
+    for (int j = 0; j < names[i].Length; j++)
+        score += (int)names[i][j] - 64;
+    sum += score * (i+1);
+    score = 0;
+}
+Console.WriteLine(sum);    
