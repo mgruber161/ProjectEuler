c9e2361a82ea2d05c0e894b51cba539881716607
Manuel <grubermk97@gmail.com>
2023-02-03 16:32:23 +0100

    solved 023

---
+++ 001-099/023/Program.cs
@@ -0,0 +1,24 @@
+﻿using EulerLib;
+int sum = 0;
+List<int> abundantNumbers = new List<int>();
+for(int i = 11;i < 28120; i++)
+    if(i.IsAbundant())
+        abundantNumbers.Add(i);
+
+for (int i = 1;i < 20162; i++)
+{
+    if(i % 2 == 0 || i > 946)
+    {
+        List<int> possibleParts = abundantNumbers.Where(p => p < i).ToList();
+        bool isSummable = false;
+        for (int j = 0; j < possibleParts.Count() && !isSummable; j++)
+            for (int k = 0; k < possibleParts.Count() && !isSummable; k++)
+                if (possibleParts[j] + possibleParts[k] == i)
+                    isSummable = true;
+        if (!isSummable)
+            sum += i;
+    }
+    else
+        sum += i;
+}
+Console.WriteLine(sum);
---
+++ EulerLib/intExt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerLib
+{
+    public static class intExt
+    {
+        public static bool IsAbundant(this int i)
+        {
+            if (i.DivisorSum() > i)
+                return true;
+            return false;
+        }
+
+        public static int DivisorSum(this int n)
+        {
+            int sum = 1;
+            for (int i = 2; i <= n / 2; i++)
+                if (n % i == 0)
+                    sum += i;
+            return sum;
+        }
+    }
+}
