998de6aae3aff3edaf936b61feb7c68607f7340b
Manuel <grubermk97@gmail.com>
2023-02-03 19:44:44 +0100

    solved 025

---
+++ 001-099/025/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+BigInteger bigInteger1 = 1;
+BigInteger bigInteger2 = 1;
+ulong index = 2;
+
+while (bigInteger1.ToString().Length < 1000 && bigInteger2.ToString().Length < 1000)
+{
+    bigInteger1 += bigInteger2;
+    bigInteger2 += bigInteger1;
+    index += 2;
+}
+
+Console.WriteLine($"{bigInteger1}\n\n{bigInteger2}\n\n{index}");
