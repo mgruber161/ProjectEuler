bfc051b5a8a0a57ca17b9be3d8b249469a210b16
Manuel <grubermk97@gmail.com>
2023-02-03 19:21:36 +0100

    solved 024

---
+++ 001-099/024/Program.cs
@@ -0,0 +1,2 @@
+﻿var lexi = EulerLib.EulerHelper.GetPermutations(new int[] {0,1,2,3,4,5,6,7,8,9}, 10).ToList();
+lexi[999_999].ToList().ForEach(x => Console.Write(x));
--- EulerLib/EulerHelper.cs
+++ EulerLib/EulerHelper.cs
@@ -61,5 +61,11 @@
                     divisors.Add(i);
             return divisors.ToArray();
         }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutations(list, length - 1).SelectMany(t => list.Where(e => !t.Contains(e)), (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
     }
 }
